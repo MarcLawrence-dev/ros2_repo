91ddd515914f92e08b3a5b75d2e3f4b84b80d64a
MarcLawrence-dev <ankledogg@gmail.com>
2025-09-19 02:39:31 +0800

    Clean repo: removes build, install, and log

--- build/ydlidar_x3_ros/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ankledogg/ydlidar_ros2_ws/src/ydlidar_x3_ros
-# Build directory: /home/ankledogg/ydlidar_ros2_ws/build/ydlidar_x3_ros
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
